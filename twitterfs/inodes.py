2cb61ffd269eae78cfb9cb81b2beda80f4cfe2aa
Maciej Małecki <maciej.malecki@hotmail.com>
2011-06-28 21:22:39 +0200

    Introduced an idea of directory getter

--- twitterfs/inodes.py
+++ twitterfs/inodes.py
@@ -44,17 +44,50 @@ class File(inode):
     
     
 class Directory(inode):
-    def __init__(self, name, mode=0755):
+    def __init__(self, name, mode=0755, getter=None, mapper=None,
+                 getter_args=[], getter_kwargs={}):
         inode.__init__(self, name, stat.S_IFDIR | mode)
         self.inodes = { }
+        self.getter = getter
+        self.getter_args = getter_args
+        self.getter_kwargs = getter_kwargs
+        self.mapper = mapper
+    
+    def _get_dynamic_inodes(self):
+        """ Basically, my idea is very cool. I think I can even state that it's
+            the best idea I've had today.
+            
+            Each Directory can have regular inodes (surprisingly, stored in
+            inodes property). But setting those inodes each time something 
+            happens in a directory would be very inconvenient. That's why it's 
+            easier to define a getter for a directory. This getter is later 
+            called when FUSE wants us to list things in a directory.
+            
+            Sometimes, getter is not enough (when getter is, say, an API, which
+            just returns some arbitrary data). That's when mapper comes in.
+            When it's not None, it gets called for every item in list, which
+            getter returns. That gives us a great degree of automation.
+            
+            Cool, ain't it?
+        """
+        
+        if self.getter is not None:
+            items = self.getter(*self.getter_args, **self.getter_kwargs)
+            return items if self.mapper is None else map(self.mapper, items)
+        else:
+            return [ ]
+        # That seems... functional.
     
     def to_stat(self):
         """ Returns Stat object describing our File """
         return Stat(st_mode=self.mode, st_nlink=2)
     
     def readdir(self):
-        """ Yields each inode, including special ones (., ..) """
+        """ Yields each inode, including special (., ..) and dynamical ones """      
         yield Directory("..")
         yield Directory(".", self.mode)
         for inode in self.inodes:
             yield inode
+        for inode in self._get_dynamic_inodes():
+            yield inode
+            
