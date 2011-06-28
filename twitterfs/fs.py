277b1a02cdcbb17abc9a982b7c0984f5196924fb
Maciej Małecki <maciej.malecki@hotmail.com>
2011-06-28 22:52:49 +0200

    FS can now read directories, also small debugging changes

--- twitterfs/fs.py
+++ twitterfs/fs.py
@@ -3,25 +3,61 @@ import errno
 import stat
 import logging
 import inodes
+import os
+
+from inodes import Directory
 
 fuse.fuse_python_api = (0, 2)
 
 class TwitterFS(fuse.Fuse):
     def __init__(self, *args, **kwargs):
-        logging.debug("TwitterFS.__init__(self, %s, %s)" % (repr(args), repr(kwargs))) 
+        logging.debug("%s.__init__(self, %s, %s)" % (repr(self),
+                                                     repr(args), 
+                                                     repr(kwargs))) 
         fuse.Fuse.__init__(self, *args, **kwargs)
-        logging.debug("Fuse.__init__'ialized") 
+        logging.debug("Fuse.__init__'ialized")
+        
+        self.main_dir = Directory("/", 0755)
+        
+        self.main_dir.inodes = { "followers" : Directory("followers", 0555),
+                                 "following" : Directory("following"),
+                                 "me" : Directory("me") }
+                                 
     
     def fsinit(self):
-        logging.debug("self.fuse_args = %s" % self.fuse_args)
+        logging.debug("%s.fuse_args = %s" % (repr(self), self.fuse_args))
         logging.info('Initializing TwitterFS at ' + self.mountpoint)
     
     def getattr(self, path):
-        logging.debug("self.getattr(self, %s)" % repr(path))
-        return inodes.Directory("/", 0755).to_stat()
+        logging.debug("%s.getattr(self, %s)" % (repr(self), repr(path)))
+        inode = self.find_inode(self.main_dir, path)
+        return inode.to_stat() if inode else -errno.ENOENT
+    
+    def find_inode(self, inode, path):
+        logging.debug("%s.find_inode(self, %s, %s)" % (repr(self), 
+                                                       repr(inode), 
+                                                       repr(path)))
+        path = path.lstrip("/")
+        if path == "":
+            return inode
+        else:
+            sep_index = path.find(os.sep)
+            if sep_index == -1:
+                return inode.inodes.get(path)
+            else:
+                name = path[:sep_index]
+                if name in inode.inodes:
+                    path = path[sep_index + 1:]
+                    return self.find_inode(inode.inodes[name], path)
     
     def readdir(self, path, offset):
-        logging.debug("self.readdir(self, %s, %s)" % (repr(path), repr(offset)))
-        for inode in inodes.Directory("/", 0755).readdir():
-            yield inode.to_direntry()
+        logging.debug("%s.readdir(self, %s, %s)" % (repr(self), 
+                                                    repr(path), 
+                                                    repr(offset)))
+        inode = self.find_inode(self.main_dir, path)
+        if inode:
+            return map(lambda i: i.to_direntry(), inode.readdir())
+        else:
+            return -errno.ENOENT
+            
         
